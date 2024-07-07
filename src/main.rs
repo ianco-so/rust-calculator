c539923ef55f98afcf85d278807f79a71d16049b
ianco <ianco.so@gmail.com>
2024-07-07 13:29:06 -0300

    :sparkles: feat: calculator completed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use meval::Expr;
+use meval::{Context, Expr};
 use std::collections::HashMap;
 use std::io::{self, Write};
 
@@ -46,13 +46,56 @@ fn handle_input(input: &str, variables: &mut HashMap<String, f64>) -> Result<Str
 }
 
 fn evaluate_expression(expr_str: &str, variables: &HashMap<String, f64>) -> Result<f64, String> {
-    let mut context = meval::Context::new();
+    let mut context = Context::new();
 
     for (key, &value) in variables.iter() {
         context.var(key, value);
     }
 
+    context.func2("mod", mod_fn);
+    context.func("fib", fib_fn);
+    context.func("abs", abs_fn);
+    context.func2("max", max_fn);
+    context.func2("min", min_fn);
+
     let expr: Expr = expr_str.parse().map_err(|_| "Invalid expression".to_string())?;
     
     expr.eval_with_context(&context).map_err(|_| "Evaluation error".to_string())
+}
+
+fn mod_fn(a: f64, b: f64) -> f64 {
+    a % b
+}
+
+fn fib_fn(n: f64) -> f64 {
+    let n = n as u32;
+    if n == 0 {
+        return 0.0;
+    } else if n == 1 {
+        return 1.0;
+    }
+
+    let mut a: u32= 0;
+    let mut b: u32 = 1;
+    let mut c: u32;
+
+    for _ in 2..=n {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    b as f64
+}
+
+fn abs_fn(a: f64) -> f64 {
+    a.abs()
+}
+
+fn max_fn(a: f64, b: f64) -> f64 {
+    a.max(b)
+}
+
+fn min_fn(a: f64, b: f64) -> f64 {
+    a.min(b)
 }
